b78916d13034f59dd2bfbbc427f8776531e71159
Jacob Rief <jacob.rief@gmail.com>
2019-05-28 23:30:16 +0200

    separate entangled and untangled fields

--- entangled/forms.py
+++ entangled/forms.py
@@ -33,28 +33,27 @@ class EntangledFormMetaclass(ModelFormMetaclass):
 
         if 'Meta' in attrs and hasattr(attrs['Meta'], 'entangled_fields'):
             entangled_fields = attrs['Meta'].entangled_fields
-            if not hasattr(attrs['Meta'], 'fields'):
-                attrs['Meta'].fields = []
-            for field_name in entangled_fields.keys():
-                attrs['Meta'].fields.append(field_name)
+            untangled_fields = list(attrs['Meta'].fields)
             attrs['formfield_callback'] = formfield_callback
         else:
-            entangled_fields = None
+            entangled_fields, untangled_fields = {}, []
         new_class = super(EntangledFormMetaclass, cls).__new__(cls, class_name, bases, attrs)
-        if entangled_fields:
-            for modelfield_name in entangled_fields.keys():
-                for field_name in entangled_fields[modelfield_name]:
-                    assert field_name in new_class.base_fields, \
-                         "Field {} listed in `{}.Meta.entangled_fields['{}']` is missing in Form declaration".format(
-                            field_name, class_name, modelfield_name)
-            new_class._meta.entangled_fields = entangled_fields
+        for modelfield_name in entangled_fields.keys():
+            for field_name in entangled_fields[modelfield_name]:
+                assert field_name in new_class.base_fields, \
+                     "Field {} listed in `{}.Meta.entangled_fields['{}']` is missing in Form declaration".format(
+                        field_name, class_name, modelfield_name)
+        for field_name in entangled_fields.keys():
+            new_class._meta.fields.append(field_name)
+        new_class._meta.entangled_fields = entangled_fields
+        new_class._meta.untangled_fields = untangled_fields
         return new_class
 
 
 class EntangledModelFormMixin(metaclass=EntangledFormMetaclass):
     def __init__(self, *args, **kwargs):
         opts = self._meta
-        if 'instance' in kwargs:
+        if 'instance' in kwargs and kwargs['instance']:
             initial = kwargs['initial'] if 'initial' in kwargs else {}
             for field_name, assigned_fields in opts.entangled_fields.items():
                 reference = getattr(kwargs['instance'], field_name)
@@ -74,7 +73,7 @@ class EntangledModelFormMixin(metaclass=EntangledFormMetaclass):
     def clean(self):
         opts = self._meta
         cleaned_data = super().clean()
-        result = {}
+        result = {f: cleaned_data[f] for f in opts.untangled_fields}
         for field_name, assigned_fields in opts.entangled_fields.items():
             result[field_name] = {}
             for af in assigned_fields:
