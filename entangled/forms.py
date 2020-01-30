2cf329af9b568b852dbc0c031aae0622fe79e164
Jacob Rief <jacob.rief@gmail.com>
2020-01-30 22:45:54 +0100

    also ctah Model lookuo error

--- entangled/forms.py
+++ entangled/forms.py
@@ -139,7 +139,7 @@ def get_related_object(scope, field_name):
     try:
         Model = apps.get_model(scope[field_name]['model'])
         relobj = Model.objects.get(pk=scope[field_name]['pk'])
-    except (KeyError, ObjectDoesNotExist, TypeError):
+    except (KeyError, LookupError, ObjectDoesNotExist, TypeError):
         relobj = None
     return relobj
 
