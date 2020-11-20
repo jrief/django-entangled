38ad0172ab6103fa96aac96e67ccc5542875fccc
Jacob Rief - Django-Migration <jacob.rief@uibk.ac.at>
2020-11-20 16:22:39 +0100

    refactor get_related_… into utils

--- entangled/forms.py
+++ entangled/forms.py
@@ -130,27 +130,3 @@ class EntangledModelForm(EntangledModelFormMixin, ModelForm):
     """
     A convenience class to create entangled model forms.
     """
-
-
-def get_related_object(scope, field_name):
-    """
-    Returns the related field, referenced by the content of a ModelChoiceField.
-    """
-    try:
-        Model = apps.get_model(scope[field_name]['model'])
-        relobj = Model.objects.get(pk=scope[field_name]['pk'])
-    except:
-        relobj = None
-    return relobj
-
-
-def get_related_queryset(scope, field_name):
-    """
-    Returns the related queryset, referenced by the content of a ModelChoiceField.
-    """
-    try:
-        Model = apps.get_model(scope[field_name]['model'])
-        queryset = Model.objects.filter(pk__in=scope[field_name]['p_keys'])
-    except (KeyError, TypeError):
-        queryset = None
-    return queryset
