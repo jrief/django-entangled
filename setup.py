fdc8e9aa5072aee2b1068823c669dc7ef2e4ccda
Fabian Braun <fsbraun@gmx.de>
2024-09-15 17:56:52 +0200

    Update tests

--- setup.py
+++ setup.py
@@ -16,13 +16,15 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
-    'Framework :: Django :: 2.2',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'Framework :: Django :: 3.2',
-    'Framework :: Django :: 4.0',
+    'Framework :: Django :: 4.2',
+    'Framework :: Django :: 5.0',
+    'Framework :: Django :: 5.1',
 ]
 
 setup(
